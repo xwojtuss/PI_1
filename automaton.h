997826ba10f509c19dedc6304a8c3c118242d918
xwojtuss <157522397+xwojtuss@users.noreply.github.com>
2024-11-08 13:36:38 +0100

    Create automaton.h

---
+++ automaton.h
@@ -0,0 +1,12 @@
+#ifndef AUTOMATON_H
+# define AUTOMATON_H
+
+# define APLHABET_AUTOMATON "01"
+
+# include <stdlib.h>
+# include <string.h>
+# include <unistd.h>
+# include <stdbool.h>
+# include <stdio.h>
+
+#endif
